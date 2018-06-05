6c7196da4ce21a785511a6c0a55dc96416fc2413
Kenia Valladarez <keniavalladarez@gmail.com>
2018-06-04 23:31:05 -0400

    Deletes extra files to have all the code in 1.

--- musicutil/musicutil.go
+++ musicutil/musicutil.go
@@ -1,4 +1,78 @@
+package musicutil
+
+import (
+	"github.com/graphql-go/graphql"
+)
+
+var (
+	OkComputer      Disc
+	TheQueenIsDead  Disc
+	BeHereNow       Disc
+	AppetiteForDest Disc
+	BackToBlack     Disc
+	HotelCal        Disc
+
+	DiscData map[int]Disc
+
+	discType *graphql.Object
+
+	MusicSchema graphql.Schema
+)
+
+type Disc struct {
+	Id     string
+	Title  string
+	Artist string
+	Year   int
+}
+
 func init() {
+	OkComputer = Disc{
+		Title:  "OK Computer",
+		Artist: "Radiohead",
+		Year:   1997,
+		Id:     "1",
+	}
+	TheQueenIsDead = Disc{
+		Title:  "The Queen is dead",
+		Artist: "The Smiths",
+		Year:   1986,
+		Id:     "2",
+	}
+	BeHereNow = Disc{
+		Title:  "Be Here Now",
+		Artist: "Oasis",
+		Year:   1997,
+		Id:     "3",
+	}
+	AppetiteForDest = Disc{
+		Title:  "Appetite for Destruction",
+		Artist: "Guns N' Roses ",
+		Year:   1987,
+		Id:     "4",
+	}
+	BackToBlack = Disc{
+		Title:  "Back To Black",
+		Artist: "Amy Winehouse",
+		Year:   2006,
+		Id:     "5",
+	}
+	HotelCal = Disc{
+		Title:  "Hotel California",
+		Artist: "Eagles",
+		Year:   1976,
+		Id:     "6",
+	}
+
+	DiscData = map[int]Disc{
+		1: OkComputer,
+		2: TheQueenIsDead,
+		3: BeHereNow,
+		4: AppetiteForDest,
+		5: BackToBlack,
+		6: HotelCal,
+	}
+
 	discType = graphql.NewObject(graphql.ObjectConfig{
 		Name:        "Disc",
 		Description: "A set of songs from one or many artists.",
