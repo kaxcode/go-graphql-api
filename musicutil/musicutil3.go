3eb3c6e5c489a8ed66192408d2740e176c689748
Kenia Valladarez <keniavalladarez@gmail.com>
2018-06-04 23:26:36 -0400

    Write graphQL object that will be queried by app.

---
+++ musicutil/musicutil3.go
@@ -0,0 +1,48 @@
+func init() {
+	discType = graphql.NewObject(graphql.ObjectConfig{
+		Name:        "Disc",
+		Description: "A set of songs from one or many artists.",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type:        graphql.NewNonNull(graphql.String),
+				Description: "The Identifier of the disc.",
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					if disc, ok := p.Source.(Disc); ok {
+						return disc.Id, nil
+					}
+					return nil, nil
+				},
+			},
+			"title": &graphql.Field{
+				Type:        graphql.String,
+				Description: "The Title of the album.",
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					if disc, ok := p.Source.(Disc); ok {
+						return disc.Title, nil
+					}
+					return nil, nil
+				},
+			},
+			"artist": &graphql.Field{
+				Type:        graphql.String,
+				Description: "The Artist of the album.",
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					if disc, ok := p.Source.(Disc); ok {
+						return disc.Artist, nil
+					}
+					return nil, nil
+				},
+			},
+			"year": &graphql.Field{
+				Type:        graphql.Int,
+				Description: "The release year.",
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					if disc, ok := p.Source.(Disc); ok {
+						return disc.Year, nil
+					}
+					return nil, nil
+				},
+			},
+		},
+	})
+}
